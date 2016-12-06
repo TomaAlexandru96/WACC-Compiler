5725a840aed0df04f1bbc69400ec912000475719
mihaivanea <mihailvanea@gmail.com>
2016-12-06 16:28:33 +0000

    [MV] updated test_extension

--- test_suite_extension.rb
+++ test_suite_extension.rb
@@ -56,6 +56,52 @@ def run_test(tests, name, correct_exitstatus)
     puts "#{name}: #{count - failed}/#{count}"
 end
 
+# emulate the program and compare outputs for differen tests
+TESTS = `find ./extension_tests/valid`
+
+def execute(program_s)
+    command = `arm-linux-gnueabi-gcc -o exe -mcpu=arm1176jzf-s -mtune=arm1176jzf-s #{program_s} && qemu-arm -L /usr/arm-linux-gnueabi exe > output.out`
+end
+
+class Fail
+    attr_reader :test
+
+    def initialize test
+        @test = test
+    end
+
+end
+
+def emulate_tests(tests)
+    puts "Emulating tests ..."
+    count = 1
+    failed = 0
+    failedTests = []
+    tests.each_line do |test|
+        test = test.chomp
+        if ((!File.directory? test) && (test.end_with? ".wacc"))
+            `./compile #{test}`
+            assembly_file = test.split('/').last.chomp("wacc").concat("s")
+            execute(assembly_file)
+            correct = `diff #{test.chomp("wacc").concat("out")} output.out | wc -c`
+            count += 1
+            if (correct.to_i != 0)
+               failedTests[failed] = Fail.new test 
+               failed += 1
+            end
+            `rm output.out && rm exe`
+        end
+    end
+    if failed != 0
+        puts "FAILED------------------------------------------------------------
+        -----------------------------------------------------------------------"
+    end
+    failedTests.each do |failedTest|
+        puts failedTest.test
+    end
+    puts "#{count - failed}/#{count}"
+end
+
 puts "Making ..."
 
 `make`
@@ -66,8 +112,10 @@ if ARGV[0] == nil
   run_test(TESTS_VALID, "Valid", 0)
   run_test(TESTS_INVALID_SEMANTIC, "Semantic", 200)
   run_test(TESTS_INVALID_SYNATX, "Syntax", 100)
+  emulate_tests(TESTS)
 elsif ARGV[0] == "valid"
   run_test(TESTS_VALID, "Valid", 0)
+  emulate_tests(TESTS)
 elsif ARGV[0] == "semantic"
   run_test(TESTS_INVALID_SEMANTIC, "Semantic", 200)
 elsif ARGV[0] == "syntax"
