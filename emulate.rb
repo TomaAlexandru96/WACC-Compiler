0b0aa04a6922bf4fce217dbc06d675c7038deac5
mihaivanea <mihailvanea@gmail.com>
2016-12-06 02:42:37 +0000

    [MV] emulate script still needs some fixes

---
+++ emulate.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/ruby
+# A script to emulate the program nad compare outputs for differen tests
+require "open3"
+TESTS = `find ./extension_tests/valid`
+
+def execute(program_s)
+    `arm-linux-gnueabi-gcc -o exe -mcpu=arm1176jzf-s -mtune=arm1176jzf-s
+    #{program_s} && qemu-arm -L /usr/arm-linux-gnueabi/exe > output.out`
+end
+
+class Fail
+    attr_reader :test
+
+    def initialize test
+        @test = test
+    end
+
+end
+
+def emulate_tests(name)
+    puts "Emulating #{name} ..."
+    count = 1
+    failed = 0
+    failedTests = []
+    TESTS.each_line do |test|
+        if ((!File.directory? test) && (test.end_with? ".wacc"))
+            expected_file = (File.dirname test).chomp("wacc").concat("out")
+            `./compile #{test}`
+            svae test strign
+            assembly_file = test.chomp("wacc").concat("s")
+            execute(ASSEMBLY_FILE)
+            correct = Open3.capture2(`diff output.out #{expected_file} | wc -c`)
+            count += 1
+            if (correct != 1)
+               failedTests[failed] = Fail.new test 
+               failed += 1
+            end
+            `rm output.out && rm exe`
+        end
+    end
+    if failed != 0
+        puts "FAILED------------------------------------------------------------
+        ------------------------------------------------------------------------
+        ------------------------------------------------------------------------
+        -----------------------------------------------------------------------"
+    end
+    failedTests.each do |failedTest|
+        puts failedTest.test
+    end
+    puts "#{name} : #{count - failed}/#{count}"
+end
+
+puts "Making ..."
+`make`
+puts "Emulating the test programs ..." 
+emulate_tests(TESTS)
